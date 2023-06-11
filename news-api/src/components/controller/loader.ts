9e1f00fa575e5bfa8916643a1381aab78c26a67a
derHerrMannelig <derherrmannelig@gmail.com>
2023-06-11 18:39:48 +0300

    feat: migrate to ts loader.ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,29 +1,37 @@
+interface Options {
+    [key: string]: string;
+}
+
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string;
+    private options: Options;
+
+    constructor(baseLink: string, options: Options) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options = {} }: { endpoint: string; options?: Options },
+        callback: () => void = () => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
+            if (res.status === 401 || res.status === 404) {
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-            throw Error(res.statusText);
+            }
+            throw new Error(res.statusText);
         }
 
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: Options, endpoint: string): string {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,13 +42,12 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load<T>(method: string, endpoint: string, callback: (data: T) => void, options: Options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
+            .then(this.errorHandler.bind(this))
             .then((res) => res.json())
             .then((data) => callback(data))
             .catch((err) => console.error(err));
     }
 }
-
 export default Loader;
