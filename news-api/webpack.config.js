2d9a6f1a498e50d90640e5051d222e679ac78eac
derHerrMannelig <derherrmannelig@gmail.com>
2023-06-11 13:05:33 +0300

    fix: add ts rule to module.rules

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -12,6 +12,10 @@ const baseConfig = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
             },
         ],
     },
@@ -28,7 +32,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' })
+        new EslintPlugin({ extensions: 'ts' }),
     ],
 };
 
