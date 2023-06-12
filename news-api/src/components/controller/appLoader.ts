113f1b20f868ffac4a64e44f9784a0b18326526d
derHerrMannelig <derherrmannelig@gmail.com>
2023-06-12 20:13:42 +0300

    fix: add news api proxy server

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '3f35e205e711486eb8ebae1d62794017',
         });
     }
